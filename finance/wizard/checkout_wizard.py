71953d90972e023e29ffad7fa5d58a596513067e
jeffery chen fan <jeffery9@gmail.com>
2018-06-15 12:30:52 +0800

    结转 增加控制，将 期间关闭 放入到 结账向导

--- finance/wizard/checkout_wizard.py
+++ finance/wizard/checkout_wizard.py
@@ -40,6 +40,22 @@ class CheckoutWizard(models.TransientModel):
         # # 月末结汇
         # exchange_wizard = self.env['create.exchange.wizard'].with_context({'no_error': True}).create({'date': self.date})
         # exchange_wizard.create_exchange()
+        self.ensure_one()
+        fixed_assets = self.env['asset'].search([
+            ('no_depreciation', '=', False),  # 提折旧的
+            ('state', '=', 'done'),  # 已确认
+            ('period_id', '!=', self.period_id.id)
+        ])
+
+        depreciations = self.env['asset.line'].search([('period_id', '!=', self.period_id.id)])
+
+        if len(depreciations) != len(fixed_assets):
+            raise UserError( u'固定资产没有计提折旧！')
+
+        depreciation_voucher = self.env['voucher.line'].search([('period_id', '=', self.period_id.id), ('name', '=',
+                                                                                                        u'固定资产折旧')])
+        if 'draft' in depreciation_voucher.mapped('state'):
+            raise UserError( u'计提折旧凭证没有确认！')
 
         need_create_exchange_voucher = False
         if self.env['finance.account'].search([('currency_id', '!=', self.env.user.company_id.currency_id.id),
